be263d10e25deced12301f617de025d5c7116398
rhas <ramonaller@gmail.com>
2023-03-28 17:32:17 +0200

    Added Restart Button.

--- script.js
+++ script.js
@@ -1,7 +1,9 @@
 const gridContainer = document.querySelector("#gridContainer")
+/// Event triggers when the window loads
 
 window.addEventListener("load", setGrid(16));
 
+/// Function triggered by previous events size =16 
 function setGrid(size){
     setGridSize(size);
     finishGrid(size);
@@ -25,4 +27,18 @@ function colorChange(e){
     const rgbGreen = Math.floor(Math.random() * 256);
     const rgbBlue = Math.floor(Math.random() * 256);
     e.target.style.backgroundColor = `rgb(${rgbRed}, ${rgbGreen}, ${rgbBlue})`;
+}
+
+/// Restart Button
+
+const restartBtn = document.getElementById("restartBtn");
+restartBtn.onclick = () => restartSketch()
+
+function restartSketch(){
+    emptyGrid();
+    setGrid(16);
+}
+
+function emptyGrid() {
+    gridContainer.innerHTML = "";
 }
